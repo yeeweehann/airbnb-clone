20e5ecd2ff897766a41cc7487f67a00f91adbb88
Hikari <yeeweehann@hotmail.com>
2018-06-08 14:45:48 +0800

    Done login

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,6 @@
+class UsersController < ApplicationController
+	before_action :require_login
+
+	def index
+	end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+class User < ApplicationRecord
+  	include Clearance::User
+  	
+  	def index
+  	end
+
+	has_many :authentications, dependent: :destroy, :listing
+
+	def self.create_with_auth_and_hash(authentication, auth_hash)
+	    user = self.create!(
+		    name: auth_hash["info"]["name"],
+		    email: auth_hash["info"]["email"],
+		    password: SecureRandom.hex(10)
+	    )
+	    user.authentications << authentication
+	    return user
+	end
+
+	 # grab google to access google for user data
+	def google_token
+		x = self.authentications.find_by(provider: 'google_oauth2')
+		return x.token unless x.nil?
+	end
+
+end
+
+
+
